a0e28c605eec238bd600e15a726710a59fdde416
Henrique-Gontijo <henriquegontijo1507@gmail.com>
2026-01-06 13:16:40 -0300

    Aula 6 - Listas e Coleções

--- bin/primeiro_dart.dart
+++ bin/primeiro_dart.dart
@@ -7,78 +7,75 @@
 
 void main() {
 
-	//? ========================== 
-	//? DEFININDO UMA FUNÇÃO SIMPLES 
-	//? ========================== 
+	//? ==================== LISTAS ====================
 
-	print("==========================\nDEFININDO UMA FUNÇÃO SIMPLES \n==========================\n");
- 
-	// Uma função simples que imprime uma mensagem 
-	void saudacao() { 
-		print('Olá, seja bem-vindo ao Dart!'); 
-	} 
- 
-	// Chamando a função 
-  	saudacao();
+	// Criando uma lista de números inteiros 
+	List<int> numeros = [10, 20, 30, 40]; 
 
+	// Acessando elementos pelo índice (começa do 0) 
+	print(numeros[0]); // 10 
 
-	//? ========================== 
-	//? FUNÇÕES COM PARÂMETROS 
-	//? ========================== 
- 
-	print("\n\n==========================\nFUNÇÕES COM PARÂMETROS\n==========================\n");
- 
-	// Função que recebe um parâmetro e exibe uma mensagem personalizada 
-	void cumprimentar(String nome) { 
-		print('Olá, $nome! Como você está?'); 
-	} 
- 
-	// Chamando a função com um argumento 
-	cumprimentar('Keila'); 
- 
-	// Função que soma dois números recebidos como parâmetros 
-	void somar(int a, int b) { 
-		int resultado = a + b; 
-		print('A soma de $a + $b é igual a $resultado'); 
-	} 
- 
-	// Chamando a função somar 
-	somar(5, 3);   
+	// Adicionando um novo elemento à lista 
+	numeros.add(50); 
+	print(numeros); // [10, 20, 30, 40, 50] 
 
-	//? ========================== 
-	//? FUNÇÕES COM RETORNO 
-	//? ========================== 
+	// Removendo um elemento 
+	numeros.remove(20); 
+	print(numeros); // [10, 30, 40, 50] 
 
-	print("\n\n==========================\nFUNÇÕES COM RETORNO \n==========================\n");
- 
-	// Função que retorna a soma de dois números 
-	int somaComRetorno(int a, int b) { 
-		return a + b; 
-	} 
- 
-	// Chamando a função e armazenando o resultado em uma variável 
-	int resultadoSoma = somaComRetorno(10, 20); 
-	print('Resultado da soma com retorno: $resultadoSoma');
+	// Iterando sobre a lista 
+	for (int numero in numeros) { 
+		print(numero); 
+	}
 
 
+	//? ==================== MAPAS ====================
 
-	//? ========================== 
-	//? FUNÇÕES ANÔNIMAS (LAMBDA) 
-	//? ==========================
-
-	print("\n\n==========================\nFUNÇÕES ANÔNIMAS (LAMBDA)\n==========================\n");
- 
-	// Uma função anônima (lambda) é uma função sem nome 
-	var multiplicar = (int x, int y) { 
-		return x * y; 
+	// Criando um mapa associando países às suas capitais 
+	Map<String, String> capitais = { 
+		'Brasil': 'Brasília', 
+		'França': 'Paris', 
+		'Japão': 'Tóquio' 
 	}; 
  
-	// Chamando a função anônima 
-	print('Multiplicação: ${multiplicar(4, 5)}'); 
- 
-	// Forma simplificada de uma função lambda (Arrow Function) 
-	var dividir = (int x, int y) => x / y; 
- 
-	// Chamando a função lambda simplificada 
-	print('Divisão: ${dividir(10, 2)}'); 
+	// Acessando um valor pela chave 
+	print(capitais['Brasil']); // Brasília 
+
+	// Adicionando um novo par chave-valor 
+	capitais['EUA'] = 'Washington, D.C.'; 
+	print(capitais); 
+
+	// Removendo um elemento 
+	capitais.remove('França'); 
+	print(capitais); 
+
+	// Iterando sobre um mapa 
+	capitais.forEach((pais, capital) { 
+		print('$pais: $capital'); 
+	});
+
+
+	//? ==================== CoONJUNTOS (Set's) ====================
+
+	// Criando um conjunto de números 
+	Set<int> numerosUnicos = {1, 2, 3, 4, 5, 5, 5}; 
+
+	// Exibindo o conjunto (valores duplicados são ignorados) 
+	print(numerosUnicos); // {1, 2, 3, 4, 5} 
+
+	// Adicionando um novo número 
+	numerosUnicos.add(6); 
+	print(numerosUnicos); // {1, 2, 3, 4, 5, 6} 
+
+	// Removendo um número 
+	numerosUnicos.remove(2); 
+	print(numerosUnicos); // {1, 3, 4, 5, 6} 
+
+	// Verificando se um número está no conjunto 
+	print(numerosUnicos.contains(3)); // true 
+
+	// Iterando sobre um conjunto 
+	for (int num in numerosUnicos) { 
+		print(num); 
+	}
 }

e893cf54abc1adbf0563c66431d2733bae78821e
Henrique-Gontijo <henriquegontijo1507@gmail.com>
2026-01-12 09:35:39 -0300

    Aula 28 - Criando um app para consumir API

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-// Execute o comando (flutter pub add http) para instalar o pacote http 
- 
 import 'package:flutter/material.dart'; 
-import 'package:http/http.dart' as http; 
-import 'dart:convert'; 
+import 'package:dio/dio.dart';
+import 'dart:math'; 
  
 void main() { 
   runApp(MyApp()); 
@@ -12,56 +10,95 @@ class MyApp extends StatelessWidget {
   @override 
   Widget build(BuildContext context) { 
     return MaterialApp( 
-      title: 'Flutter API Example', 
-      theme: ThemeData( 
-        primarySwatch: Colors.blue, 
-      ), 
-      home: ApiExamplePage(), 
+      title: 'Imagens Aleatórias', 
+      home: ImageScreen(),
+      debugShowCheckedModeBanner: false,
     ); 
   } 
-} 
+}
+
+class ImageModel {
+  final String imageUrl;
+
+  ImageModel({required this.imageUrl});
+
+  factory ImageModel.fromJson(Map<String, dynamic> json) {
+    return ImageModel (
+        imageUrl: "https://picsum.photos/id/${json['id']}/500/300",
+      );
+  }
+}
  
-class ApiExamplePage extends StatefulWidget { 
+class ImageScreen extends StatefulWidget {
+  const ImageScreen({super.key});
+
   @override 
-  _ApiExamplePageState createState() => _ApiExamplePageState(); 
-} 
- 
-class _ApiExamplePageState extends State<ApiExamplePage> { 
-  String _data = ""; 
- 
-  Future<void> fetchData() async { 
-    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1')); 
-     
-    if (response.statusCode == 200) { 
-      setState(() { 
-        _data = json.decode(response.body)['title']; 
-      }); 
-    } else { 
-      setState(() { 
-        _data = 'Erro ao carregar os dados'; 
-      }); 
-    } 
-  } 
-  @override 
-  Widget build(BuildContext context) { 
-    return Scaffold( appBar: AppBar(title: Text('Consumo de API no Flutter')), 
-      body: Center( 
-        child: Column( 
-          mainAxisAlignment: MainAxisAlignment.center, 
-          children: <Widget>[ 
-            Text(
-              _data, 
-              textAlign: TextAlign.center, 
-              style: TextStyle(fontSize: 18)
-            ), 
-            SizedBox(height: 20), 
-            ElevatedButton( 
-              onPressed: fetchData, 
-              child: Text('Buscar Dados'), 
-            ), 
-          ], 
-        ), 
-      ), 
-    ); 
-  } 
-} 
+  _ImageScreenState createState() => _ImageScreenState();
+}
+
+class _ImageScreenState extends State<ImageScreen> {
+  final Dio _dio = Dio();
+  final String _apiUrl = 'https://picsum.photos/v2/list?page=1&limit10';
+  List<ImageModel> _images = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadImages();
+  }
+
+  Future<void> _loadImages() async {
+    setState(() => _isLoading = true);
+    int randomPage = Random().nextInt(100) + 1; // Gera um número de página aleatória entre 1 e 100
+
+    try {
+      final response = await _dio.get('https://picsum.photos/v2/list?page=$randomPage&limit=8');
+      if (response.statusCode == 200) {
+        List<dynamic> data = response.data;
+        setState(() {
+          _images = data.map((json) => ImageModel.fromJson(json)).toList();
+          _isLoading = false;
+        });
+      }
+    } catch (e) {
+      setState(() => _isLoading = false);
+      print("Erro ao carregar imagens: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Imagens Aleatórias")),
+      body: _isLoading
+      ? const Center(child: CircularProgressIndicator())
+      : GridView.builder(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemCount: _images.length,
+        itemBuilder:(context, index) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child : Image.network(
+              _images[index].imageUrl,
+              fit: BoxFit.cover,
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress == null) return child;
+                return const Center(child: Icon(Icons.error, size: 50));
+              }
+            )
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _loadImages,
+        child: const Icon(Icons.refresh),
+      ),
+    );
+  }
+}
